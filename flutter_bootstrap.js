bfc938bd771da8a1169104222428bbc2559057db
hidecard <hidecard1500@gmail.com>
2026-04-03 19:20:49 +0630

    fixa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2067879334" /* Simplified service worker */
+    serviceWorkerVersion: null /* Disable service worker */
   }
 });
